0615fd2b9b0fe59633508ffa01685d86fa8dbefe
Malte Weyrich <malte.weyrich@hotmail.de>
2024-04-13 11:49:04 +0200

    Easy link to goodnotes dir

--- scripts/sync_goodnotes.sh
+++ scripts/sync_goodnotes.sh
@@ -2,4 +2,4 @@
 
 rsync -av ~/OneDriver/03_GoodNotes/01_Studium/4.Semester ~/documents/studium_lokal/4.Semester
 
-rsync -av ~/OneDriver/03_GoodNotes/01_Studium/AtomicNotes ~/documents/vimwiki_para/assets/AtomicNotes/
+rsync -av ~/OneDriver/03_GoodNotes/01_Studium/AtomicNotes ~/documents/vimwiki_para/assets/
