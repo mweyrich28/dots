880439eebf02e160991925d123b4c96e024a2700
Malte Weyrich <malte.weyrich@hotmail.de>
2024-04-15 21:47:12 +0200

    updating vimwiki_backlinks

--- nvim/lua/user/vimwiki_backlinks.lua
+++ nvim/lua/user/vimwiki_backlinks.lua
@@ -1,34 +1,21 @@
-local pickers = require "telescope.pickers"
-local finders = require "telescope.finders"
-local conf = require("telescope.config").values
-local actions = require "telescope.actions"
-local action_state = require "telescope.actions.state"
+local telescope = require("telescope.builtin")
 
 local function search_backlinks()
     local current_file = vim.fn.expand('%:t')
     -- Remove .* extension
     current_file = current_file:gsub(".md", "")
-    local search_pattern = "'\\[*\\]\\(.*" .. current_file .. "[.md\\)|\\)']"
+    local search_pattern = "\\[*\\]\\(.*" .. current_file .. "[.md\\)|\\)]"
 
-    local opts = require("telescope.themes").get_dropdown { prompt_title = "Vimwiki Backink" }
-    local results = vim.fn.systemlist("rg " .. search_pattern)
-    pickers.new(opts, {
-        finder = finders.new_table({
-            results = results
-        }),
-        sorter = conf.file_sorter(opts),
-        attach_mappings = function(prompt_bufnr, map)
-            actions.select_default:replace(function()
-                local selection = action_state.get_selected_entry()
-                vim.cmd("edit " .. selection.value) -- Open the selected file
-                actions.close(prompt_bufnr)
-            end)
-            return true
-        end
-    }):find()
+    telescope.live_grep({
+        prompt_title = "Wiki Backlinks",
+        default_text = search_pattern,
+        no_ignore = true,
+        hidden = true,
+    })
 end
 
 
 return {
     search_backlinks = search_backlinks,
 }
+
