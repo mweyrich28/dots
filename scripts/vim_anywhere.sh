45cd526c41248b29e5de85fa24ccc55b8c7d5b8c
Malte Weyrich <malte.weyrich@hotmail.de>
2024-10-02 15:57:19 +0200

    adding border

--- scripts/vim_anywhere.sh
+++ scripts/vim_anywhere.sh
@@ -2,7 +2,7 @@
 
 # header for latex doc
 HEADER="
-\documentclass[border=0pt,preview]{standalone}  
+\documentclass[border=20pt,preview]{standalone}  
 \usepackage{amsmath}  
 \begin{document}  
 
@@ -31,7 +31,7 @@ done
 
 xelatex -interaction=nonstopmode "$TMPFILE" # compile
 pdftoppm -png -r 350 "$PDF" "$TMPDIR/image" # to png
-convert "$PNG" -trim +repage "$PNG" # crop png
+convert "$PNG" -trim -bordercolor White -border 10x10 +repage "$PNG"
 xclip -selection clipboard -t image/png -i "$PNG" # save to clipboard
 
 # Cleanup
