e53d002e94632f01d3534d107bc4b394f636e5eb
Malte Weyrich <malte.weyrich@hotmail.de>
2025-01-04 16:47:06 +0100

    fixing forced delay in lualine

--- nvim/lua/user/lualine.lua
+++ nvim/lua/user/lualine.lua
@@ -32,9 +32,9 @@ require('lualine').setup {
         always_divide_middle = true,
         globalstatus = false,
         refresh = {
-            statusline = 1000,
-            tabline = 1000,
-            winbar = 1000,
+            statusline = 100,
+            tabline = 100,
+            winbar = 100,
         }
     },
     sections = {
