050c11a7d459eba90785514d4e7ab71f4fa5ad57
Malte Weyrich <120316140+mweyrich28@users.noreply.github.com>
2026-03-19 16:37:13 +0100

    fixing spaces in path

--- scripts/rofi_recent_files.sh
+++ scripts/rofi_recent_files.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/sh
 grep -oP '(?<=file://)([^<]+)' ~/.local/share/recently-used.xbel | \
-cut -f1 -d" " | sed 's/"//' | \
-sort | uniq | \
+cut -f1 -d"\"" | sed "s/\%20/\\\ /g" |  \
 while read f; do [ -f "$f" ] && echo "$f"; done | \
-awk -F'/' '{print $NF"                                                               |"$0}' | \
+awk -F'/' '{print $NF"|"$0}' | \
 rofi -dmenu -i -p 'Recent Files' -display-columns 1 | \
 cut -d'|' -f2 | \
 xargs -I{} xdg-open '{}'
