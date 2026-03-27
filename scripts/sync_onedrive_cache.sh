b7fa7d57226492e050939f8ac8821761f03eed84
Malte Weyrich <120316140+mweyrich28@users.noreply.github.com>
2026-03-27 20:07:32 +0100

    update

--- scripts/sync_onedrive_cache.sh
+++ scripts/sync_onedrive_cache.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/bash
+rclone lsf -R onedrive: --files-only \
+--exclude "01_Dokumente/02_Notes/**" \
+--exclude "01_Dokumente/04_Passwords/**" \
+--exclude "01_Dokumente/03_Bildung/02_Schule/**" \
+--exclude "03_GoodNotes/**" \
+--exclude "02_QuickAccess/**" \
+--exclude "04_Bilder/**" \
+--exclude "Persönlicher Tresor/**" \
+--exclude ".xdg-volume-info" \
+--exclude ".Trash**" | sort > ~/.custom_cache/buff.txt
+
 # update cache only if sucess
-rclone lsf -R onedrive:01_Dokumente --files-only | sort > ~/.custom_cache/buff.txt
 if [ ${PIPESTATUS[0]} -eq 0 ]; then
     mv ~/.custom_cache/buff.txt ~/.custom_cache/onedrive_cache.txt
 else
--- scripts/tmp_download.sh
+++ scripts/tmp_download.sh
@@ -5,25 +5,13 @@ if [[ ! -d "$TMP_DEPOSIT" ]]; then
     mkdir "$TMP_DEPOSIT"
 fi
 
-# mapfile -t FILESTODOWNLOAD < <(rclone lsf -R $PREF --files-only \
-# --exclude "01_Dokumente/02_Notes/**" \
-# --exclude "01_Dokumente/04_Passwords/**" \
-# --exclude "01_Dokumente/03_Bildung/02_Schule/**" \
-# --exclude "03_GoodNotes/**" \
-# --exclude "02_QuickAccess/**" \
-# --exclude "04_Bilder/**" \
-# --exclude "Persönlicher Tresor/**" \
-# --exclude ".xdg-volume-info" \
-# --exclude ".Trash**" \
-# | fzf -m --bind 'tab:toggle+down,shift-tab:toggle+up' --preview 'echo "Files to download:"; echo; nl -w2 -s". " {+f}')
-
 mapfile -t FILESTODOWNLOAD < <(cat ~/.custom_cache/onedrive_cache.txt \
 | fzf -m --bind 'tab:toggle+down,shift-tab:toggle+up' --preview 'echo "Files to download:"; echo; nl -w2 -s". " {+f}')
 
 for file in "${FILESTODOWNLOAD[@]}"; do
-    rclone copy "$PREF$file" "$TMP_DEPOSIT" --progress
+    rclone copy "$PREF$file" "$TMP_DEPOSIT" & # 
 done
 
 if [[ ${FILESTODOWNLOAD[@]} ]]; then
-    xdg-open "$TMP_DEPOSIT"&
+    xdg-open "$TMP_DEPOSIT"> /dev/null 2>&1 &
 fi
